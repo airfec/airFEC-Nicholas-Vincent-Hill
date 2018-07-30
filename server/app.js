a8cd79a2a51a698ae142a80bd478895b6aeec569
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-30 16:32:28 -0700

    Add react and microservices

--- server/app.js
+++ server/app.js
@@ -1,6 +1,53 @@
 const express = require("express");
+const proxy = require("http-proxy-middleware");
+const path = require("path");
 const app = express();
+
+app.get("/", function(req, res) {
+  res.redirect("/rooms/1");
+});
 
 app.use(express.static("public"));
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.get("/rooms/:id", function(req, res) {
+  const reactPath = path.join(__dirname, "../public/index.html");
+  res.sendFile(reactPath);
+});
+
+// booking component
+app.use(
+  "/api/rooms/:id/bookings",
+  proxy({
+    target: "http://127.0.0.1:3001"
+    // changeOrigin: true
+  })
+);
+
+// photo component
+app.use(
+  "/api/rooms/:id/photos",
+  proxy({
+    target: "http://127.0.0.1:3004",
+    changeOrigin: true
+  })
+);
+
+// review component
+app.use(
+  "/api/rooms/:id/reviews",
+  proxy({
+    target: "http://127.0.0.1:3002"
+    // changeOrigin: true
+  })
+);
+
+// info component
+app.use(
+  "/api/rooms/:id",
+  proxy({
+    target: "http://127.0.0.1:3003"
+    // changeOrigin: true
+  })
+);
+
+app.listen(3000, () => console.log("Proxy listening on port 3000!"));
